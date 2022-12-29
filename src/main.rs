5bd0a7be069e4b3be642d68c39e6bdeb202cfe86
erickdevv <111036821+ErickDevv@users.noreply.github.com>
2022-12-29 16:08:17 -0600

    reading and listing data (db.json)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use ncurses::*;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    name: String,
+    description: String,
+    completed: bool,
+}
 
 const REGULAR: i16 = 0;
 const SELECTED: i16 = 1;
@@ -14,15 +25,32 @@ fn main() {
     init_pair(REGULAR, COLOR_WHITE, COLOR_BLACK);
     init_pair(SELECTED, COLOR_BLACK, COLOR_WHITE);
 
-    let menu: [&str; 5] = ["Menu: ", "List", "Add", "Edit", "Remove"];
+    let menu: [&str; 6] = ["Menu: ", "List", "Add", "Edit", "Remove", "Exit"];
+    let mut repeat = true;
 
-    let option = crate_menu(&mut menu.to_vec());
+    while repeat == true {
+        clear();
+        let option = crate_menu(&mut menu.to_vec());
 
-    if option == 1 {
-        list_menu();
+        if option == 1 {
+            list_menu();
+        } else if option == 5 {
+            repeat = false;
+        }
     }
 
     endwin();
+}
+
+fn read_db() -> Vec<Task> {
+    let mut file = fs::File::open("src/db.json").expect("file not found");
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let tasks: Vec<Task> = serde_json::from_str(&contents).expect("JSON was not well-formatted");
+    tasks
 }
 
 fn list_menu() -> u32 {
@@ -30,12 +58,17 @@ fn list_menu() -> u32 {
     clear();
     noecho();
 
-    let menu: [&str; 4] = ["Options: ", "All", "Completed", "Incompleted"];
+    let menu: [&str; 4] = ["Options: ", "All", "Completed", "Pending"];
     let option = crate_menu(&mut menu.to_vec());
 
     if option == 1 {
         clear();
-        addstr(&format!(" Tasks:     "));
+        let tasks: Vec<Task> = read_db();
+        for task in tasks {
+            addstr(&task.name);
+            addstr("\n");
+        }
+        refresh();
         getch();
     }
     1
